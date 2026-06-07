39f08035b53e32a2439cbcddfe95e7856e648674
guy oron <goron@redhat.com>
2026-06-07 20:52:18 +0300

    Add test feature for QualityFlow e2e generation

---
+++ test-feature.go
@@ -0,0 +1,8 @@
+package main
+
+// Placeholder feature file for QualityFlow integration test.
+// QualityFlow will read this PR diff and generate e2e tests for it.
+
+func handleTestFeature() error {
+	return nil
+}
